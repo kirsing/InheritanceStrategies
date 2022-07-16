3debe95c0adabaf008ad48bc47dd788eb26207e1
kirsing <kirsing98@icloud.com>
2022-07-16 12:21:59 +0300

    initial commit

---
+++ src/main/java/com/example/inheritancestrategies/InheritancestrategiesApplication.java
@@ -0,0 +1,13 @@
+package com.example.inheritancestrategies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InheritancestrategiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InheritancestrategiesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/inheritancestrategies/InheritancestrategiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inheritancestrategies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InheritancestrategiesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
